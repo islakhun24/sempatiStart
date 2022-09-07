3fed6e00a2da4e29ce132773e33fb5d4519be64b
fame46 <fahmiyahya46@gmail.com>
2022-09-07 08:46:44 +0700

    fame46: laporan progress

--- app/src/main/java/sempati/star/app/fragment/LaporanManifestFragment.java
+++ app/src/main/java/sempati/star/app/fragment/LaporanManifestFragment.java
@@ -3,20 +3,19 @@ package sempati.star.app.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.tabs.TabLayout;
-
 import sempati.star.app.R;
-import sempati.star.app.adapter.ViewPagerAdapterLihatMenu;
-import sempati.star.app.databinding.FragmentBookingBinding;
-import sempati.star.app.databinding.FragmentManifestBinding;
 
-public class ManifestFrag extends Fragment implements TabLayout.BaseOnTabSelectedListener{
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LaporanManifestFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LaporanManifestFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +26,7 @@ public class ManifestFrag extends Fragment implements TabLayout.BaseOnTabSelecte
     private String mParam1;
     private String mParam2;
 
-    public ManifestFrag() {
+    public LaporanManifestFragment() {
         // Required empty public constructor
     }
 
@@ -37,11 +36,11 @@ public class ManifestFrag extends Fragment implements TabLayout.BaseOnTabSelecte
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ManifestFrag.
+     * @return A new instance of fragment LaporanManifestFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ManifestFrag newInstance(String param1, String param2) {
-        ManifestFrag fragment = new ManifestFrag();
+    public static LaporanManifestFragment newInstance(String param1, String param2) {
+        LaporanManifestFragment fragment = new LaporanManifestFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,37 +57,10 @@ public class ManifestFrag extends Fragment implements TabLayout.BaseOnTabSelecte
         }
     }
 
-    FragmentManifestBinding v;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        v = FragmentManifestBinding.inflate(inflater, container, false);
-
-        v.tabLayout.addTab(v.tabLayout.newTab().setText("TRANSAKSI"));
-        v.tabLayout.addTab(v.tabLayout.newTab().setText("MANIFEST"));
-        v.tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        ViewPagerAdapterLihatMenu adapter = new ViewPagerAdapterLihatMenu(getActivity().getSupportFragmentManager(), v.tabLayout.getTabCount());
-        v.viewpager.setAdapter(adapter);
-        v.tabLayout.setOnTabSelectedListener(this);
-
-        return v.getRoot();
-    }
-
-    @Override
-    public void onTabSelected(TabLayout.Tab tab) {
-        v.viewpager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(TabLayout.Tab tab) {
-
-    }
-
-    @Override
-    public void onTabReselected(TabLayout.Tab tab) {
-
+        return inflater.inflate(R.layout.fragment_laporan_manifest, container, false);
     }
 }
